3601055eccae9254742c0b85004fa1961e7e810e
lemotw <lemotw1024@gmail.com>
2026-08-03 03:51:38 +0800

    feat: complete earlier sections when reading ahead

--- app/web/sw.js
+++ app/web/sw.js
@@ -8,7 +8,7 @@
  */
 'use strict';
 
-const CACHE_VERSION = 'v9';
+const CACHE_VERSION = 'v10';
 const STATIC_CACHE = 'learning-static-' + CACHE_VERSION;
 const CONTENT_CACHE = 'learning-content';
 
