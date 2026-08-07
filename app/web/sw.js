cbc5364aa2bef56ffe8374887f4323719b65107d
lemotw <lemotw1024@gmail.com>
2026-08-07 19:56:24 +0800

    fix: distinguish tutor and learner message backgrounds

--- app/web/sw.js
+++ app/web/sw.js
@@ -8,7 +8,7 @@
  */
 'use strict';
 
-const CACHE_VERSION = 'v10';
+const CACHE_VERSION = 'v11';
 const STATIC_CACHE = 'learning-static-' + CACHE_VERSION;
 const CONTENT_CACHE = 'learning-content';
 
